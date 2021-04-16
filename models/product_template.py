969ae3085ab13cb89db2b5fb88391e0beddc883d
Cyril VINH-TUNG <cyril@invitu.com>
2021-04-15 19:33:26 -1000

    [DEL] Remove unsued fields in product_template

--- models/product_template.py
+++ models/product_template.py
@@ -10,12 +10,3 @@ class ProductTemplate(models.Model):
     est_contenant = fields.Boolean(string='Est un contenant',
                                    help='Cochez cette case si cet article est un contenant')
     id_revatua = fields.Integer(string='Id Revatua', help='Identifiant Revatua')
-    weight_uom = fields.Many2one(comodel_name='uom.uom',
-                                 string='Unité de mesure de poids',
-                                 help='Unité de mesure de poids',
-                                 domain="[('category_id', '=', 'Weight')]")
-    volume_uom = fields.Many2one(comodel_name='uom.uom',
-                                 string='Unité de mesure de volume',
-                                 help='Unité de mesure de volume',
-                                 domain="[('category_id', '=', 'Volume')]")
-    prix_volume = fields.Integer(string='Prix au volume', help='Prix au volume')
