c7ce03c6cee37978ba5620f3f6dc663c13fe85b4
Cyril VINH-TUNG <cyril@invitu.com>
2022-06-16 14:33:21 -1000

    [IMP] Add UserError log for helping support

--- models/revatua_api.py
+++ models/revatua_api.py
@@ -34,13 +34,13 @@ class RevatuaApi(models.Model):
         try:
             r = requests.post(url_request, json=data, verify=False, headers=headers)
             if r.status_code == 500:
-                raise UserError(_('Erreur Revatua Internal Server Error 500 : Contactez la DPAM !'))
+                raise UserError(_('Erreur Revatua Internal Server Error 500 sur %s : Contactez la DPAM !') % (url_request))
             elif r.status_code != 201:
-                raise UserError(_('Message Revatua %s - Detail %s - Error %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code))
+                raise UserError(_('Message Revatua %s - Detail %s - Error %s sur %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code, url_request))
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
             _logger.exception(e)
-            raise UserError(_('We had trouble to create your data, please retry later or contact your support if the problem persists - Network Error'))
+            raise UserError(_('We had trouble to create your data, please retry later or contact your support if the problem persists - Network Error sur %s') % (url_request))
         return r
 
     def api_put(self, url, data):
@@ -49,13 +49,13 @@ class RevatuaApi(models.Model):
         try:
             r = requests.put(url_request, json=data, verify=False, headers=headers)
             if r.status_code == 500:
-                raise UserError(_('Erreur Revatua Internal Server Error 500 : Contactez la DPAM !'))
+                raise UserError(_('Erreur Revatua Internal Server Error 500 sur %s : Contactez la DPAM !') % (url_request))
             elif r.status_code != 200:
-                raise UserError(_('Message Revatua %s - Detail %s - Error %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code))
+                raise UserError(_('Message Revatua %s - Detail %s - Error %s sur %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code, url_request))
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
             _logger.exception(e)
-            raise UserError(_('We had trouble to update your data, please retry later or contact your support if the problem persists - Network Error'))
+            raise UserError(_('We had trouble to create your data, please retry later or contact your support if the problem persists - Network Error sur %s') % (url_request))
         return r
 
     def api_patch(self, url, data):
@@ -64,13 +64,13 @@ class RevatuaApi(models.Model):
         try:
             r = requests.patch(url_request, json=data, verify=False, headers=headers)
             if r.status_code == 500:
-                raise UserError(_('Erreur Revatua Internal Server Error 500 : Contactez la DPAM !'))
+                raise UserError(_('Erreur Revatua Internal Server Error 500 sur %s : Contactez la DPAM !') % (url_request))
             elif r.status_code != 200:
-                raise UserError(_('Message Revatua %s - Detail %s - Error %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code))
+                raise UserError(_('Message Revatua %s - Detail %s - Error %s sur %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code, url_request))
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
             _logger.exception(e)
-            raise UserError(_('We had trouble to cancel your data, please retry later or contact your support if the problem persists - Network Error'))
+            raise UserError(_('We had trouble to create your data, please retry later or contact your support if the problem persists - Network Error sur %s') % (url_request))
         return r
 
     def api_get(self, url):
@@ -79,11 +79,11 @@ class RevatuaApi(models.Model):
         try:
             r = requests.get(url_request, verify=False, headers=headers)
             if r.status_code == 500:
-                raise UserError(_('Erreur Revatua Internal Server Error 500 : Contactez la DPAM !'))
+                raise UserError(_('Erreur Revatua Internal Server Error 500 sur %s : Contactez la DPAM !') % (url_request))
             elif r.status_code != 200:
-                raise UserError(_('Message Revatua %s - Detail %s - Error %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code))
+                raise UserError(_('Message Revatua %s - Detail %s - Error %s sur %s : Contactez la DPAM !') % (r.json().get('message'), r.json().get('detail'), r.status_code, url_request))
             r.raise_for_status()
         except requests.exceptions.RequestException as e:
             _logger.exception(e)
-            raise UserError(_('We had trouble to read your data, please retry later or contact your support if the problem persists - Network Error'))
+            raise UserError(_('We had trouble to create your data, please retry later or contact your support if the problem persists - Network Error sur %s') % (url_request))
         return r
